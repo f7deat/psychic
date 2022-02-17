548e0ca03f7352de771624b8c598fbdb5505f811
f7deat <tan.dct9x@gmail.com>
2022-02-17 19:28:14 +0700

    add tailwild

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="flex">
+      <div className='w-24 h-screen border-r flex flex-col justify-center items-center'>
+        <div className='h-12 w-12 bg-dark rounded-2xl'></div>
+      </div>
+      <header className="App-header flex-grow">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -19,6 +22,23 @@ function App() {
           Learn React
         </a>
       </header>
+      <div className='w-1/4 h-screen p-6'>
+        <div className='flex justify-between items-center'>
+          <h1 className='text-3xl py-6 mb-4'>My Profile</h1>
+          <button className='h-12 w-12 rounded-full border hover:bg-slate-100 flex items-center justify-center text-slate-500'>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+            </svg>
+          </button>
+        </div>
+        <div className='flex justify-center flex-col items-center'>
+          <div className='h-40 w-40 bg-gadient rounded-full p-1 mb-4'>
+            <div className='w-full h-full bg-white rounded-full'></div>
+          </div>
+          <div className='text-3xl mb-2'>Đinh Công Tân</div>
+          <div className='text-gray-500'>f7deat@gmail.com</div>
+        </div>
+      </div>
     </div>
   );
 }
